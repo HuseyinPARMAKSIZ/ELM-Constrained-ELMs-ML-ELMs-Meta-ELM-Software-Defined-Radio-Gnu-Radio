e3ceff16bdaa47d33075366037fdfb8915bd2ab0
h4ck3r <47844849+HuseyinPARMAKSIZ@users.noreply.github.com>
2023-03-03 17:58:05 +0300

    Update Separation.m

--- Separation.m
+++ Separation.m
@@ -13,3 +13,16 @@ load('Ready6Ftrvete.mat');
     train_data = RFkayit(train_idx, :);
     % select test data
     test_data = RFkayit(test_idx, :);
+
+-------------------------------------------------------------
+clear all
+load('RF_020323.mat');
+
+% randomly select indexes to split data into 70% 
+    % training set, 0% validation set and 30% test set.
+    [train_idx, ~, test_idx] = dividerand(2443, 0.7, 0, 0.3);
+    % slice training data with train indexes 
+    %(take training indexes in all 10 features)
+    train_data = RF010323all(train_idx, :);
+    % select test data
+    test_data = RF010323all(test_idx, :);
